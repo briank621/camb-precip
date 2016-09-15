3ef5cbaa3e1d5284c6807ce1041ee394301230cf
wjung2017-1 <wjung2017@gmail.com>
2016-09-15 23:54:29 +0900

    Adding Plots of Yearly Precip

--- precipAmount.py
+++ precipAmount.py
@@ -9,11 +9,11 @@ import DrawBarGraph
 
 latRange = [5,20]
 lonRange = [90,120]
-models = ['csiro-mk3-6-0']
-#models = ['noresm1-m', 'cnrm-cm5', 'bnu-esm', 'ccsm4', 'hadgem2-es']
+#models = ['csiro-mk3-6-0']
+models = ['noresm1-m', 'cnrm-cm5', 'bnu-esm', 'ccsm4', 'hadgem2-es']
 var = 'pr'
 ensemble = [1]
-# ensemble = range(1, 11)
+#ensemble = range(1, 11)
 monthStart = 1
 monthEnd = 12
 # monthsStart = [12, 3, 6, 9]
@@ -22,10 +22,31 @@ yearsum=0
 yearsum_old=[]
 yearsum_new=[]
 
+years = range(1985,2005)
+
+for m in models:
+	print m
+	yearsum_old=[]
+	for i in range(1985,2005):
+		sumOverTimeBase = AreaAvgOverTime.AreaSumOverTime(var, [m], ensemble, [i,i+1], latRange, lonRange, monthStart, monthEnd)
+
+		if var == 'pr':
+			for i in range(0,len(sumOverTimeBase[2])):
+				sumOverTimeBase[2][i]*=60*60*24
+
+		tmp=0
+		for j in range(len(sumOverTimeBase[2])):
+			tmp+=sumOverTimeBase[2][j]
+		yearsum_old.append(tmp)
+
+	plt.figure()
+	DrawBarGraph.draw(years,yearsum_old,"Precipitation Amount Yearly","Year (%s)"%(m)," Amount (mm)")
+
 years = range(2045,2065)
 
 for m in models:
 	print m
+	yearsum_new=[]
 	for i in range(2045,2065):
 		sumOverTimeBase = AreaAvgOverTime.AreaSumOverTime(var, [m], ensemble, [i,i+1], latRange, lonRange, monthStart, monthEnd)
 
@@ -33,14 +54,13 @@ for m in models:
 			for i in range(0,len(sumOverTimeBase[2])):
 				sumOverTimeBase[2][i]*=60*60*24
 
-		print sumOverTimeBase[2]
-
 		tmp=0
 		for j in range(len(sumOverTimeBase[2])):
 			tmp+=sumOverTimeBase[2][j]
-		yearsum_old.append(tmp)
+		yearsum_new.append(tmp)
 
-print yearsum_old
+	plt.figure()
+	DrawBarGraph.draw(years,yearsum_new,"Precipitation Amount Yearly","Year (%s)"%(m)," Amount (mm)")
 
-DrawBarGraph.draw(years,yearsum_old,"Precipitation Amount Yearly","Year","Amount (mm)")
+
 plt.show()
